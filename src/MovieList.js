4ee45ee29afecb6a53aaa4137d72585e2a65628d
Scott <scott289@gmail.com>
2021-01-28 15:53:43 -0800

    change watchlist to favlist, added color icons

--- src/MovieList.js
+++ src/MovieList.js
@@ -7,17 +7,19 @@ import loadingImg from "./images/loading.gif";
 export default function MovieList() {
   const { data: movies, loading, error } = useRequest(getMovieListUrl());
 
-  const [watchlist, setWatchlist] = useState([0]);
+  const [favlist, setFavlist] = useState([0]);
 
   useEffect(() => {
-    setWatchlistFromLocal()
+    setFavlistFromLocal()
   }, [])
 
-  const setWatchlistFromLocal = () => {
-    const localList = localStorage.getItem("watchlist");
+  const setFavlistFromLocal = () => {
+    const localList = localStorage.getItem("favlist");
     try {
-      console.log("Loading watchlist list from local")
-      setWatchlist(JSON.parse(localList));
+      if (localList) {
+        console.log("Loading favlist list from local, setting to: ", localList)
+        setFavlist(JSON.parse(localList))
+      }
     } catch (e) {
       console.warn("Failed to parse localStorage localList:", localList)
     }
@@ -29,58 +31,64 @@ export default function MovieList() {
     window.location.reload();
   };
 
-  const watchlistUpdate = async (id, isAdd=true) => {
+  const favlistUpdate = async (id, isAdd=true) => {
     let newWatchlist = null
     if (isAdd) {
       console.log("ADD IT!")
-      newWatchlist = [...watchlist, id];
+      newWatchlist = [...favlist, id];
     } else {
       console.log("REMOVE IT!")
-      newWatchlist = watchlist.filter(x => x !== id);
+      newWatchlist = favlist.filter(x => x !== id);
     }
 
     if (newWatchlist) {
       console.log("UPDATE BOTH TO ", newWatchlist)
-      setWatchlist(newWatchlist)
-      localStorage.setItem("watchlist", JSON.stringify(newWatchlist));
+      setFavlist(newWatchlist)
+      localStorage.setItem("favlist", JSON.stringify(newWatchlist));
     }
   }
 
-  const doWatchlistClick = async (id) => {
-    const index = watchlist.indexOf(id);
-    console.log("doWatchlistClick handling", {watchlist, id, index})
+  const doFavlistClick = async (id) => {
+    console.log("doWatchlistClick handling", {favlist, id})
+
+
+
+    const index = favlist.indexOf(id);
+    console.log("index", index)
 
     // if its already there, remove it, otherwise add it
-    if (index > -1) await watchlistUpdate(id, false);
-    else await watchlistUpdate(id);
+    if (index > -1) await favlistUpdate(id, false);
+    else await favlistUpdate(id);
   };
 
   return (
     <main>
       <h1>Movies</h1>
       {loading && <img src={loadingImg} />}
-      <MovieTable {...{movies, doDelete, doWatchlistClick}} />
+      <MovieTable {...{movies, doDelete, doFavlistClick, favlist}} />
     </main>
   );
 }
 
 
-function MovieTable({movies, doDelete, doWatchlistClick}) {
+function MovieTable({movies, doDelete, doFavlistClick, favlist}) {
   return (
     <table>
       <thead>
       <tr>
+        <th>id</th>
         <th>title</th>
         <th>year</th>
         <th>stars</th>
         <th>imdb</th>
         <th>delete</th>
-        <th>watchlist</th>
+        <th>favlist</th>
       </tr>
       </thead>
       <tbody>
       {movies.length > 0 && movies.map((movie) => (
         <tr key={movie.id}>
+          <td>{movie.id}</td>
           <td><Link to={`/detail?id=${movie.id}`}>{movie.title}</Link></td>
           <td>{movie.year}</td>
           <td>{movie.imdb_stars}</td>
@@ -99,7 +107,8 @@ function MovieTable({movies, doDelete, doWatchlistClick}) {
             </button>
           </td>
           <td>
-            <button onClick={() => doWatchlistClick(movie.id)}>WATCHLIST</button>
+            {favlist.includes(movie.id) && <button style={{backgroundColor: 'green'}} onClick={() => doFavlistClick(movie.id)}> - </button>}
+            {! favlist.includes(movie.id) && <button style={{backgroundColor: 'red'}} onClick={() => doFavlistClick(movie.id)}> + </button>}
           </td>
         </tr>
       ))}
